d4ddbab23bd2641862a1020217f0775faedc315a
acydxnm <73566961+acydxnm@users.noreply.github.com>
2025-05-09 15:20:42 +0800

    Update index.component.ts

--- src/components/search/index.component.ts
+++ src/components/search/index.component.ts
@@ -3,27 +3,27 @@
 
 import {
   Component, Input, ViewChild, ElementRef, AfterViewInit, Renderer2
-} from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import {
   getDefaultSearchEngine,
   setDefaultSearchEngine,
   queryString,
   isDark,
   isMobile,
-} from 'src/utils'
-import { Router, ActivatedRoute } from '@angular/router'
-import { search } from 'src/store'
-import type { ISearchItemProps } from 'src/types'
-import { SearchType } from './index'
-import { $t } from 'src/locale'
-import { NzInputModule } from 'ng-zorro-antd/input'
-import { NzPopoverModule } from 'ng-zorro-antd/popover'
-import { NzSelectModule } from 'ng-zorro-antd/select'
-import { LogoComponent } from 'src/components/logo/logo.component'
-import { isLogin } from 'src/utils/user'
-import event from 'src/utils/mitt'
+} from 'src/utils';
+import { Router, ActivatedRoute } from '@angular/router';
+import { search } from 'src/store';
+import type { ISearchItemProps } from 'src/types';
+import { SearchType } from './index';
+import { $t } from 'src/locale';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { LogoComponent } from 'src/components/logo/logo.component';
+import { isLogin } from 'src/utils/user';
+import event from 'src/utils/mitt';
 
 @Component({
   standalone: true,
@@ -40,81 +40,78 @@ import event from 'src/utils/mitt'
   styleUrls: ['./index.component.scss'],
 })
 export class SearchComponent implements AfterViewInit {
-  @ViewChild('input', { static: false }) input!: ElementRef
-  @Input() size: 'small' | 'default' | 'large' = 'large'
-  @Input() showLogo = true
+  @ViewChild('input', { static: false }) input!: ElementRef;
+  @Input() size: 'small' | 'default' | 'large' = 'large';
+  @Input() showLogo = true;
 
-  readonly $t = $t
-  readonly isLogin = isLogin
-  readonly SearchType = SearchType
-  readonly searchEngineList: ISearchItemProps[] = search.list.filter(
-    (item) => !item.blocked,
-  )
-  readonly search = search
-  isDark = isDark()
-  currentEngine: ISearchItemProps = getDefaultSearchEngine()
-  searchTypeValue = Number(queryString()['type']) || SearchType.All
-  keyword = queryString().q
+  readonly $t = $t;
+  readonly isLogin = isLogin;
+  readonly SearchType = SearchType;
+  readonly search = search;
+  readonly searchEngineList: ISearchItemProps[] = search.list.filter(item => !item.blocked);
+
+  currentEngine: ISearchItemProps = getDefaultSearchEngine();
+  searchTypeValue = Number(queryString()['type']) || SearchType.All;
+  keyword = queryString().q;
+  isDark = isDark();
+
+  inputReady = false; // ✅ 控制 readonly 解除
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private renderer: Renderer2 // ✅ 新增
+    private renderer: Renderer2
   ) {
-    event.on('SEARCH_FOCUS', () => {
-      this.inputFocus()
-    })
+    event.on('SEARCH_FOCUS', () => this.inputFocus());
 
     if (!this.isLogin && this.searchTypeValue === SearchType.Id) {
-      this.searchTypeValue = SearchType.All
+      this.searchTypeValue = SearchType.All;
     }
 
     event.on('EVENT_DARK', (isDark: unknown) => {
-      this.isDark = isDark as boolean
-    })
-  }
-
-  // ✅ 用户操作时才允许触发聚焦
-  private inputFocus() {
-    setTimeout(() => {
-      this.input?.nativeElement?.focus()
-    }, 100)
+      this.isDark = isDark as boolean;
+    });
   }
 
   ngAfterViewInit() {
-    // ✅ 延迟恢复交互能力
     setTimeout(() => {
-      this.renderer.addClass(this.input.nativeElement, 'ready')
-    }, 400)
+      this.inputReady = true;
+      this.renderer.addClass(this.input.nativeElement, 'ready');
+      this.renderer.removeAttribute(this.input.nativeElement, 'readonly');
+    }, 400);
   }
 
   get logoImage() {
-    return this.isDark
-      ? search.darkLogo || search.logo
-      : search.logo || search.darkLogo
+    return this.isDark ? search.darkLogo || search.logo : search.logo || search.darkLogo;
+  }
+
+  private inputFocus() {
+    setTimeout(() => {
+      this.input?.nativeElement?.focus();
+    }, 100);
   }
 
   onSelectChange() {
-    this.inputFocus()
+    this.inputFocus();
   }
 
   clickEngineItem(index: number) {
-    document.body.click()
-    this.currentEngine = this.searchEngineList[index]
-    this.inputFocus()
-    setDefaultSearchEngine(this.currentEngine)
+    document.body.click();
+    this.currentEngine = this.searchEngineList[index];
+    this.inputFocus();
+    setDefaultSearchEngine(this.currentEngine);
   }
 
   triggerSearch() {
     if (this.currentEngine.url) {
       const url = this.currentEngine.url.includes('${q}')
         ? this.currentEngine.url.replaceAll('${q}', this.keyword)
-        : this.currentEngine.url + this.keyword
-      window.open(url)
-      return
+        : this.currentEngine.url + this.keyword;
+      window.open(url);
+      return;
     }
 
-    const params = queryString()
+    const params = queryString();
     this.router.navigate([], {
       relativeTo: this.activatedRoute,
       queryParams: {
@@ -123,16 +120,16 @@ export class SearchComponent implements AfterViewInit {
         type: this.searchTypeValue,
         _: Date.now(),
       },
-    })
+    });
 
     if (isMobile()) {
-      this.input?.nativeElement?.blur()
+      this.input?.nativeElement?.blur();
     }
   }
 
   onKey(event: KeyboardEvent) {
     if (event.code === 'Enter') {
-      this.triggerSearch()
+      this.triggerSearch();
     }
   }
 }
